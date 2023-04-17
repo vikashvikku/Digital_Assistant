72fc7fdad0360d62b9bcb3407d2a5017cc611139
Vikash Kumar <vikkuvikash79097@gmail.com>
2023-04-17 13:56:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+// import "./App.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortonaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Digital Assistant</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@Alexa099"
+                image={AlexaImage}
+                description="Alexa is Amazon's cloud-based voice service available on hundreds of millions of devices from Amazon and third-party device manufacturers."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortona"
+                handle="@cortona68"
+                image={CortonaImage}
+                description="Cortana is Microsoft's personal productivity assistant that helps you save time and focus attention on what matters most."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri091"
+                image={SiriImage}
+                description="Siri is Apple's virtual assistant for iOS, macOS, tvOS and watchOS devices that uses voice recognition and is powered by artificial intelligence (AI)."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/ProfileCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import "./ProfileCard.css";
+
+const ProfileCard = ({ title, handle, image, description }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img className="image" src={image} alt="" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+          <div className="content">{description}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
